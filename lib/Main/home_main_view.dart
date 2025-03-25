d40b01465e8c9524800a17df129fb22c80097a16
ali4hassan9ali2022 <kasrawyali47@gmail.com>
2025-03-25 15:14:22 +0200

    create my app

---
+++ lib/Main/home_main_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class HomeMainView extends StatelessWidget {
+  const HomeMainView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
