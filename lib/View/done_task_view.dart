4d26e4f6e6f8c7e4cf016088a07859e8450a093f
ali4hassan9ali2022 <kasrawyali47@gmail.com>
2025-03-25 15:30:12 +0200

    create done and archived view

---
+++ lib/View/done_task_view.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:to_do_app/Widgets/custom_no_tasks.dart';
+
+class DoneTaskView extends StatelessWidget {
+  const DoneTaskView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const CsutomNoTasks();
+  }
+}
