7427b4e138d9bc680899191dcac58b5eee8aaedd
ali4hassan9ali2022 <kasrawyali47@gmail.com>
2025-03-25 17:05:47 +0200

    create file simple bloc opserver

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:to_do_app/Cubit/create_databaes/create_cubit.dart';
 import 'package:to_do_app/Main/home_main_view.dart';
+import 'package:to_do_app/simple_bloc_opserver.dart';
 
 void main() {
+  Bloc.observer = SimpleBlocOpserver();
   runApp(const ToDoApp());
 }
 
---
+++ lib/simple_bloc_opserver.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/widgets.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class SimpleBlocOpserver implements BlocObserver {
+  @override
+  void onChange(BlocBase bloc, Change change) {
+    debugPrint("Change = $change");
+  }
+
+  @override
+  void onClose(BlocBase bloc) {
+    debugPrint("Close = $bloc");
+  }
+
+  @override
+  void onCreate(BlocBase bloc) {
+    debugPrint("Create = $bloc");
+  }
+
+  @override
+  void onError(BlocBase bloc, Object error, StackTrace stackTrace) {
+    debugPrint('onError -- ${bloc.runtimeType}, $error');
+  }
+
+  @override
+  void onEvent(Bloc bloc, Object? event) {
+    
+  }
+
+  @override
+  void onTransition(Bloc bloc, Transition transition) {
+    // TODO: implement onTransition
+  }
+}
