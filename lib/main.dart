e8b56ebab968685b358eee2dbaff422d2fc6c4bb
ali4hassan9ali2022 <kasrawyali47@gmail.com>
2025-03-26 08:43:07 +0200

    add file shared perferences to my app

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:to_do_app/Cubit/DataBase/database_cubit.dart';
 import 'package:to_do_app/Main/home_main_view.dart';
+import 'package:to_do_app/cache/cache_helper.dart';
 import 'package:to_do_app/simple_bloc_opserver.dart';
 
 void main() {
   Bloc.observer = SimpleBlocOpserver();
+  CacheHelper().init();
   runApp(const ToDoApp());
 }
 
